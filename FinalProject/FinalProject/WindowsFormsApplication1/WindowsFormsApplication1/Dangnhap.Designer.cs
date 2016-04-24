d5339737e4eff1ace9da8edf253f5c9f2ff87993
TrinhHan <ttngochan83@gmail.com>
2016-04-24 23:07:47 +0700

    V1.0
    New

---
+++ FinalProject/FinalProject/WindowsFormsApplication1/WindowsFormsApplication1/Dangnhap.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Dangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1_Taikhoản = new System.Windows.Forms.Label();
+            this.label2_Matkhau = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2_Matkhau);
+            this.groupBox1.Controls.Add(this.label1_Taikhoản);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btn_Thoat);
+            this.groupBox1.Controls.Add(this.btnDangNhap);
+            this.groupBox1.Location = new System.Drawing.Point(59, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(441, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(138, 127);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 0;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Location = new System.Drawing.Point(271, 127);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(75, 23);
+            this.btn_Thoat.TabIndex = 1;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(186, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1_Taikhoản
+            // 
+            this.label1_Taikhoản.AutoSize = true;
+            this.label1_Taikhoản.Location = new System.Drawing.Point(98, 26);
+            this.label1_Taikhoản.Name = "label1_Taikhoản";
+            this.label1_Taikhoản.Size = new System.Drawing.Size(55, 13);
+            this.label1_Taikhoản.TabIndex = 4;
+            this.label1_Taikhoản.Text = "Tài khoản";
+            // 
+            // label2_Matkhau
+            // 
+            this.label2_Matkhau.AutoSize = true;
+            this.label2_Matkhau.Location = new System.Drawing.Point(98, 64);
+            this.label2_Matkhau.Name = "label2_Matkhau";
+            this.label2_Matkhau.Size = new System.Drawing.Size(52, 13);
+            this.label2_Matkhau.TabIndex = 5;
+            this.label2_Matkhau.Text = "Mật khẩu";
+            // 
+            // Dangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Dangnhap";
+            this.Text = "DangNhap";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2_Matkhau;
+        private System.Windows.Forms.Label label1_Taikhoản;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
+
